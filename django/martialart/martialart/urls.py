7b21a32863b83b7da4ce634f175ef6c84dc0f5d5
Badsha1996 <43995221+Badsha1996@users.noreply.github.com>
2019-02-05 16:09:49 +0530

    websites

--- django/martialart/martialart/urls.py
+++ django/martialart/martialart/urls.py
@@ -2,19 +2,26 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tutorials/',include('tutorials.urls')),
     path('about/',views.about),
-    path('',views.homepage)
-
+    path('',views.homepage,name="index"),
 ]
 
 
 
 
-urlpatterns+= staticfiles_urlpatterns()
+urlpatterns+=staticfiles_urlpatterns()
+urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
 
